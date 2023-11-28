0c229bec15348859a8267b2084d3b12c75770dd5
Sirivella Ram Sandeep <NH2605@nslhub.in>
2023-11-28 13:10:41 +0530

    Added the logic for nested questions

--- lms_main.py
+++ lms_main.py
@@ -5,13 +5,13 @@ import inspect
 import redis
 import re
 
-r = redis.Redis(host='localhost', port=6379, db=1, decode_responses=True)
+r = redis.Redis(host='127.0.0.1', port=6379, db=1, decode_responses=True)
 
 app = FastAPI()
 
 class Data(BaseModel):
     auth_token: str
-    user_resp : str
+    user_resp : str 
 
 
 
@@ -19,33 +19,39 @@ invalidResponseQuestions={"start_date": "please enter in following format(YYYY-M
                "end_date": "please enter in following format (YYYY-MM-DD)",
                "reason": "Please mention the reason",
                "leave_type": "Please enter either consolidated or special only",
-               "partial_leaves": "Any partial leaves?",
-               "partial_leaves_yes":["please enter the date in following format ","please enter the time(forenoon or Afternoon)"],
+               "apply_partial_leave": "Any partial leaves?",
+               "partial_leaves_date":"please enter the date in following format((YYYY-MM-DD))",
+               "partial_leaves_time":"please enter the time(forenoon or Afternoon)",
                "no_leaves": "please enter the numerical value ",
                "attachment": "Any attachments?"}
 
-regexforEachQuestions={"start_date": re.compile("^(([0-9]{2})?[0-9]{2})(\/|-)(1[0-2]|0?[1-9])\2(3[01]|[12][0-9]|0?[1-9])$"),
-               "end_date":re.compile("^(([0-9]{2})?[0-9]{2})(\/|-)(1[0-2]|0?[1-9])\2(3[01]|[12][0-9]|0?[1-9])$"),
+regexforEachQuestions={"start_date": re.compile(r"(([0-9]{4}))(\/|-)(1[0-2]|0?[1-9])\3(3[01]|[12][0-9]|0?[1-9])"),
+               "end_date":re.compile(r"(([0-9]{4}))(\/|-)(1[0-2]|0?[1-9])\3(3[01]|[12][0-9]|0?[1-9])"),
                "reason": re.compile("[a-zA-Z]+"),
                "leave_type": re.compile(r'consolidated|special', re.IGNORECASE),
-               "partial_leaves": "Any partial leaves?",
+               "apply_partial_leave": re.compile(r'yes|No', re.IGNORECASE),
+               "partial_leaves_date":re.compile(r"(([0-9]{4}))(\/|-)(1[0-2]|0?[1-9])\3(3[01]|[12][0-9]|0?[1-9])"),
+               "partial_leaves_time":re.compile(r'forenoon|Afternoon', re.IGNORECASE),
                "no_leaves": re.compile("[0-9]+"),
-               "attachment": "Any attachments?"
+               "attachment": re.compile("[a-zA-Z]+")
 
 }
+questionsList=["start_date","end_date","reason","leave_type","apply_partial_leave","partial_leaves_date","partial_leaves_time","no_leaves","attachment"]
 
 
 
-extractedData=""
+
+
 
 def validate_the_response(key,response):
-    pattermatch=regexforEachQuestions[key].findall(response)
+    pattermatch=regexforEachQuestions[key].search(response)
     if(pattermatch):
         extractedData=pattermatch.group()
-        return True
+        return [True,extractedData]
+    
+    return [False]
 
 
-    return False
 
 def get_all(user_id):
     response = r.hgetall(user_id)
@@ -55,6 +61,8 @@ def store(user_id, arg, response):
     curr_value = get_all(user_id)
     curr_value[arg] = response
     r.hset(user_id, mapping=curr_value)
+    print(curr_value,"redi store")
+
 
 def create_user(user_id):
     mapping = {
@@ -62,9 +70,12 @@ def create_user(user_id):
         "end_date": "",
         "reason": "",
         "leave_type": "",
-        "partial_leaves": "",
+        "apply_partial_leave":"",
+        "partial_leaves_date":"",
+        "partial_leaves_time":"",
         "no_leaves": "",
-        "attachment": ""
+        "attachment":"",
+        "index":0
     }
     r.hset(user_id, mapping=mapping)
 
@@ -75,21 +86,42 @@ def get_question_for_key(key):
                "end_date": "What is the end date? (YYYY-MM-DD)",
                "reason": "Please mention the reason",
                "leave_type": "Do you want to apply for consolidated or special leave?",
-               "partial_leaves": "Any partial leaves?",
-               "partial_leaves_yes":["please enter the date in following format ","please enter the time(forenoon or Afternoon)"],
+               "apply_partial_leave": "Any partial leaves?",
+               "partial_leaves_date":"Enter the date in following format (YYYY-MM-DD)",
+                "partial_leaves_time":"Specify the time (forenoon or Afternoon)",
                "no_leaves": "What is the number of leaves?",
                "attachment": "Any attachments?"}
     return mapping[key]
 
+
+def get_curr_param(user_id):
+    mapping = get_all(user_id)
+    ind=int(mapping['index'])
+    return questionsList[ind-1]
+
+#O(1) operation 
 def get_next_param(user_id):
     mapping = get_all(user_id)
-    for key in mapping:
-        if mapping[key] == "":
-            return key
-    return None
+    ind=int(mapping['index'])
+
+    if(ind==5):
+        if mapping['apply_partial_leave']=="No":
+            ind=ind+2
+
+    mapping["index"]=ind+1
+    r.hset(user_id, mapping=mapping)
+
+    if ind>=len(questionsList):
+        return None
+    
+    
+    return questionsList[ind]
+
 
 def get_next_question(user_id):
+    
     param = get_next_param(user_id)
+    
     if param:
         question = get_question_for_key(param)
         return question
@@ -109,18 +141,22 @@ def user_exists(user_id):
 async def health_check():
     return {'health_status': 'ok'}
 
-@app.get("/apply_leave")
+@app.post("/apply_leave")
 async def apply_leave(data: Data):
     user_id = data.auth_token
     user_resp = data.user_resp
-    if user_exists(user_id):                                       
-        param = get_next_param(user_id)
-        if(validate_the_response(param,user_resp)):
-            store(user_id, param, extractedData)
+
+    if user_exists(user_id):                                 
+        param = get_curr_param(user_id)
+        patternMatch=validate_the_response(param,user_resp)
+        if(patternMatch[0]):
+           store(user_id, param, patternMatch[1])
         else:
             return invalidResponseQuestions[param]
     else:
         create_user(user_id)
+ 
+
     next_question = get_next_question(user_id)
     return next_question
 
